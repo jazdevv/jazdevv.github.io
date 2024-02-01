174d03bc047c93bf878a4c20796e0451a076bde0
j <joelpepa2@gmail.com>
2024-02-01 12:36:21 +0100

    add: optimize videos

---
+++ js/homeMap.js
@@ -0,0 +1,10 @@
+var map = L.map('map',{
+    center: [51.505, -0.09],
+    zoom: 3,
+    minZoom: 3,
+    zoomControl: false
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
