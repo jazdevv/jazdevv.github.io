c4815ab10edc45f0de5e41daa6a70d978be23c3f
j <joelpepa2@gmail.com>
2024-02-02 08:44:12 +0100

    refactor folder structure

--- js/renderNavbar.js
+++ js/renderNavbar.js
@@ -1,4 +1,4 @@
-fetch('../templates/navbar.html')
+fetch('../components/navbar.html')
         .then(response => response.text())
         .then(html => {
             document.getElementById('navbar').innerHTML = html;
