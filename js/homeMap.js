e64c51b6e277f69a5e1a93e164f52d421d6eacdd
j <joelpepa2@gmail.com>
2024-02-04 20:52:12 +0100

    add: campaign markers

--- js/homeMap.js
+++ js/homeMap.js
@@ -1,11 +1,38 @@
-var map = L.map('map',{
-    center: [51.505, -0.09],
-    zoom: 3,
-    minZoom: 3,
-    maxZoom: 6,
-    zoomControl: false
-});
+async function getCampaigns() {
+    const campaignsJsonUnparsed = await fetch('../data/campaigns.json')
+    const campaignsJson = await campaignsJsonUnparsed.json()
+    
+    return campaignsJson;
+}
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+async function loadMap() {
+    var map = L.map('map', {
+        center: [51.505, -0.09],
+        zoom: 2,
+        minZoom: 2,
+        maxZoom: 6,
+        zoomControl: false
+    });
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    //render campaigns
+    const campaigns = await getCampaigns()
+
+    campaigns.campaigns.map( (campaign ) => {
+        const marker = L.marker(campaign.geoCoordinates, {
+            title: campaign.name
+        }).addTo(map); 
+
+        //redirect to campaign page on marker click
+        marker.on('click',()=>{
+            window.open(`/campaign.html?id=${campaign.id}`)
+        })
+    })
+    
+}
+
+loadMap()
+
