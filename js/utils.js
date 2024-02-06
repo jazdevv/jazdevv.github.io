7691c7c1978598596f76ae68220fbb280aa77f0b
j <joelpepa2@gmail.com>
2024-02-06 20:57:25 +0100

    add: add news utils

--- js/utils.js
+++ js/utils.js
@@ -8,10 +8,10 @@ class CampaignsUtilsClass {
         return campaignsJson.campaigns;
     }
 
-    async getCampaign(campaignId){
+    async getCampaign(campaignId) {
         const campaigns = await this.getCampaigns()
-        for (let campaign of campaigns){
-            if (campaign.id == campaignId){
+        for (let campaign of campaigns) {
+            if (campaign.id == campaignId) {
                 return campaign
             }
         }
@@ -43,7 +43,7 @@ class CampaignsUtilsClass {
         let renderedTemplates = "";
         for (let campaign of campaigns) {
 
-        
+
             // parse the HTML string into a DOM element
             const parser = new DOMParser();
             const doc = parser.parseFromString(cardTemplate, 'text/html');
@@ -55,7 +55,64 @@ class CampaignsUtilsClass {
             const imageUrl = '../images/campaigns/' + campaign.image;
             doc.querySelector('.campaign-card img').setAttribute('src', imageUrl);
             const detailsUrl = '/campaign.html?id=' + campaign.id;
-            doc.querySelector('.campaign-detail').setAttribute('href',detailsUrl);
+            doc.querySelector('.campaign-detail').setAttribute('href', detailsUrl);
+
+            // extract the modified HTML
+            const modifiedTemplate = doc.body.innerHTML;
+
+            renderedTemplates = renderedTemplates + modifiedTemplate;
+        }
+
+        return renderedTemplates;
+    }
+
+}
+
+class NewsUtilsClass {
+    async getNews() {
+        const newsJsonUnparsed = await fetch('../data/news.json')
+        const newsJson = await newsJsonUnparsed.json()
+
+        return newsJson.news;
+    }
+
+    async getNew(newId) {
+        const news = await this.getNews()
+        for (let newNotice of news) {
+            if (newNotice.id == newId) {
+                return newNotice
+            }
+        }
+        return undefined;
+    }
+
+    async getAmountOfNews(amount) {
+        const news = await this.getNews();
+
+        const resultArray = [];
+        for (let i = 0; i <= amount -1; i++) {
+            resultArray.push(news[i]);
+        }
+
+        return resultArray;
+    }
+
+    async convertNewsToCard(news) {
+        const cardTemplate = await (await fetch("../components/templates/newCard.html")).text()
+        let renderedTemplates = "";
+        for (let newNotice of news) {   
+            // parse the HTML string into a DOM element
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(cardTemplate, 'text/html');
+
+            // modify content
+            const customPageUrl = "/new.html?id=" + newNotice.id;
+            doc.querySelector('.new-card').setAttribute('href', customPageUrl);
+            doc.querySelector('.new-card h3').textContent = newNotice.title;
+            doc.querySelector('.new-card p').textContent = newNotice.miniDescription;
+            const imageUrl = '../images/news/' + newNotice.image;
+            doc.querySelector('.new-card img').setAttribute('src', imageUrl);
+
 
             // extract the modified HTML
             const modifiedTemplate = doc.body.innerHTML;
@@ -69,4 +126,5 @@ class CampaignsUtilsClass {
 }
 
 // define global classes
-const CampaignsUtils = new CampaignsUtilsClass()
+const CampaignsUtils = new CampaignsUtilsClass();
+const NewsUtils = new NewsUtilsClass();
