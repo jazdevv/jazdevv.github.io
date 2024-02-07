d53ade115a77e6d96636eb7b92e367c31eef5012
j <joelpepa2@gmail.com>
2024-02-07 13:21:43 +0100

    add: newPage render

--- js/renderContent/renderNewPage.js
+++ js/renderContent/renderNewPage.js
@@ -3,6 +3,13 @@ async function renderContent() {
     const news = await NewsUtils.getNew(newsId)
     
     console.log(news)
+    document.getElementById('new-title').textContent = news.title;
+    document.getElementById('new-author').textContent = news.author;
+    document.getElementById('new-date').textContent = news.date;
+    const imageUrl = '../images/news/' + news.image;
+    document.querySelector('.new-page img').setAttribute('src',imageUrl)
+    document.querySelector('.new-page > h3').textContent = news.miniDescription;
+    document.querySelector('.new-page > p').textContent = news.description;
     // document.querySelector('.news-header-block div h1').textContent = news.name;
     // document.querySelector('.news-header-block div p').textContent = news.description;
     // const imageUrl = '../images/newss/' + news.image;
